d327e09ec7934e6e8bf3f7bfd9d6f06d67acb3e6
skand406 <chlxx0040639@gmail.com>
2025-10-09 22:49:59 +0900

    fix: 카카오 api를 vworld api로 변경

--- src/map/map.controller.ts
+++ src/map/map.controller.ts
@@ -4,11 +4,11 @@ import { GeocodeRequestDto } from './dto/geocode-request.dto';
 
 @Controller('map') // 프론트엔드가 요청할 경로: /api/geocode
 export class MapController {
-  constructor(private readonly geocodingService: MapService) {}
+  constructor(private readonly mapService: MapService) {}
 
   @Post('geocode')
-  async geocodeAddress(@Body() dto: GeocodeRequestDto) {
-    const { lat, lng } = await this.geocodingService.getCoordinates(dto.address);
+  async geocodeAddress(@Body() geocodeRequestDto: GeocodeRequestDto) {
+    const { lat, lng } = await this.mapService.getCoordinates(geocodeRequestDto.address);
     
     // ✅ 프론트엔드로 위도/경도 데이터 반환
     return { lat, lng };
--- src/map/map.service.ts
+++ src/map/map.service.ts
@@ -1,42 +1,32 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { HttpException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 
 // ✅ 카카오 REST API 키는 서버 환경 변수에서 가져옵니다.
-const KAKAO_REST_API_KEY = process.env.KAKAO_REST_API_KEY ;
-const KAKAO_GEOCODE_URL = 'https://dapi.kakao.com/v2/local/search/address.json';
+
 
 @Injectable()
 export class MapService {
   constructor(private readonly httpService: HttpService) {}
+  private readonly apiUrl = process.env.apiUrl ?? 'https://apis.vworld.kr/new2coord.do';
+  private readonly apiKey = process.env.apiKey;
 
-  async getCoordinates(address: string): Promise<{ lat: number; lng: number }> {
-    try {
-      const response = await firstValueFrom(
-        this.httpService.get(KAKAO_GEOCODE_URL, {
-          params: { query: address },
-          headers: {
-            // ✅ 서버에서 안전하게 REST API 키를 사용
-            Authorization: `KakaoAK ${KAKAO_REST_API_KEY}`,
-          },
-        }),
-      );
+  async getCoordinates(address: string) {
+    const encoded_address = encodeURIComponent(address);
 
-      const document = response.data.documents[0];
-      
-      if (!document) {
-        throw new InternalServerErrorException('좌표를 찾을 수 없는 주소입니다.');
-      }
+    const params = {
+      q: encoded_address,
+      output: 'json',
+      epsg: 'epsg:4326',
+      domain: process.env.BACK_URL,
+      apiKey: this.apiKey
+    };
 
-      // 경도(x)와 위도(y)를 반환
-      return {
-        lat: parseFloat(document.y),
-        lng: parseFloat(document.x),
-      };
-    } catch (error) {
-      console.error('카카오 지오코딩 오류:', error.response?.data || error.message);
-      // ✅ 403 Forbidden 에러도 여기서 서버 에러로 처리
-      throw new InternalServerErrorException('좌표 변환 서비스에 문제가 발생했습니다.');
+    try{
+      const response = await firstValueFrom(this.httpService.get(this.apiUrl,{params}));
+      return response.data;
+    } catch (err){
+      throw err;
     }
   }
 }
