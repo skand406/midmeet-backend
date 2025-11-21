47c0c4e2f97b716b2e793f36d0c4976fbcffc104
Chohyeon Kim <chohyeonK3@gmail.com>
2025-11-21 14:43:54 +0900

    feat: db party:mid_lat,mid_lng,mid_place 추가

---
+++ prisma/migrations/20251120115901_init/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `Party` ADD COLUMN `mid_lat` DECIMAL(9, 6) NULL,
+    ADD COLUMN `mid_lng` DECIMAL(9, 6) NULL;
---
+++ prisma/migrations/20251121054307_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Party` ADD COLUMN `mid_place` VARCHAR(191) NULL;
